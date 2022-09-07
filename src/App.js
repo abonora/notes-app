c27e412dfc86317e8d601d4a35a1935a3b84786c
albertob <alberto.bonora@gmail.com>
2022-09-07 11:45:40 -0400

    added SASS support

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Notes</h1>
+        <p>Let's create a simple notes app that accepts a Title (input) and a body (textarea) and saves it in localstorage. Let's break it down into these steps to build it:</p>
+        <ol>
+          <li>Get the basic component that has the input fields working and storing the data in localstorage</li>
+          <li>Once we verify that the notes are being stored in localstorage, create a new component that displays all the notes</li>
+          <li>Style the whole thing</li>
+        </ol>
       </header>
     </div>
   );
